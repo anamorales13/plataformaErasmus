476cadc5745a2271831db7f46c454e22e517af9b
anamorales13 <maldonado.morales@alu.uhu.es>
2020-10-18 19:50:39 +0200

    cambios de conexcion mongose

--- Backend/index.js
+++ Backend/index.js
@@ -5,18 +5,20 @@
 var mongoose = require('mongoose');
 
 var app=require('./app');
-app.set('port', process.env.PORT || 3900);
+
+const PORT = process.env.PORT || 3900;
 //var port= 3900;
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb+srv://anamorales13:13Abril04a>@plataforma.2cxua.mongodb.net/<dbname>?retryWrites=true&w=majority',{ useUnifiedTopology: true, useNewUrlParser: true})
+mongoose.connect('mongodb+srv://anamorales13:13Abril04a@plataforma.2cxua.mongodb.net/test',{ useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify:false, useCreateIndex: true})
         .then(()=>{
             console.log('La conexion a la BD se ha realizado con exito');
 
-            app.listen(app.get('port'), ()=> {
-                console.log('servidor corriendo en http://localhost:'+port);
+            app.listen(PORT, ()=> {
+               console.log('servidor corriendo en http://localhost:'+ PORT);
     
             });
-        });
+        })
+        .catch((err) => console.log(err));
