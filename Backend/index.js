28250b6f4dc83d978edf425f5eaf85d69d5f6f51
anamorales13 <maldonado.morales@alu.uhu.es>
2020-10-21 19:25:00 +0200

    nuevo todo

--- Backend/index.js
+++ Backend/index.js
@@ -13,13 +13,13 @@ const PORT = process.env.PORT || 3900;
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 
-const MONGODB_URI="mongodb+srv://anamorales13:hNcazIGjCMBPeZPl@plataforma.2cxua.mongodb.net/plataformaErasmus?retryWrites=true&w=majority"
+//const MONGODB_URI="mongodb+srv://anamorales13:hNcazIGjCMBPeZPl@plataforma.2cxua.mongodb.net/test?retryWrites=true&w=majority"
 
 
 app.set('port', process.env.PORT);
 console.log(PORT);
 
-mongoose.connect("mongodb+srv://anamorales13:hNcazIGjCMBPeZPl@plataforma.2cxua.mongodb.net/plataformaErasmus?retryWrites=true&w=majority" ,{ useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify:false, useCreateIndex: true})
+mongoose.connect("mongodb+srv://anamorales13:gsRdIpvYCjOPvXwd@plataforma.2cxua.mongodb.net/<dbname>?retryWrites=true&w=majority" ,{ useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify:false, useCreateIndex: true})
         .then(()=>{
             console.log('La conexion a la BD se ha realizado con exito');
 
