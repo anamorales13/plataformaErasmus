e21bf35bbf40f0cc7476ce162db4c956b3052d0e
anamorales13 <maldonado.morales@alu.uhu.es>
2020-10-22 18:38:19 +0200

    ya no se

--- Backend/index.js
+++ Backend/index.js
@@ -17,17 +17,19 @@ mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 
  
-mongoose.connect(process.env.MONGO_URI,{ useUnifiedTopology: true, useNewUrlParser: true})
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/baseerasmus",{ useUnifiedTopology: true, useNewUrlParser: true})
         .then(()=>{
             console.log('La conexion a la BD se ha realizado con exito');
 
            
         });
 
+    
+
 //LEER LOCAL HOST DE VARIABLE Y PUERTOS
 
 const host= process.env.HOST || '0.0.0.0';
-const port= process.env.PORT || 3900;
+const port= process.env.PORT || 3900 ;
 
 
 app.listen(port, host, ()=> {
